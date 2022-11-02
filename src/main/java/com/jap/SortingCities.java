796ff1eeb614f47fb7796448a8c64aac38c0b67d
@ajaydixit598 <ajaydixit598@gmail.com>
2022-11-02 13:20:39 +0530

    find city from zurich completed

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -4,29 +4,29 @@ package com.jap;
  * Hello world!
  *
  */
-public class SortingCities
-{
-    public static void main( String[] args )
-    {
-        String []cityName={"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch" };
-        String[] cityDistance={ "138" , "52" , "118" , "136" ,"85" ,"276" ,"103" ,"87" ,"214" ,"101"};
-        SortingCities sortingCities=new SortingCities();
+public class SortingCities {
+    public static void main(String[] args) {
+        String[] cityName = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch" };
+        String[] cityDistance = {"138", "52", "118", "136", "85", "276", "103", "87", "214", "101" };
+        SortingCities sortingCities = new SortingCities();
         System.out.println("The city Name in Upper Case : ");
-        for (int index = 0; index <cityName.length ; index++) {
+        for (int index = 0; index < cityName.length; index++) {
 
-            String upperCaseCityName=sortingCities.convertToUpperCase(cityName[index]);
-            System.out.print( " " +upperCaseCityName +",");
+            String upperCaseCityName = sortingCities.convertToUpperCase(cityName[index]);
+            System.out.print(" " + upperCaseCityName + ",");
         }
 
-           String cityNameDistanceFromZurich= sortingCities.findTheCityDistanceFromZurich52(cityName,cityDistance);
-
-        System.out.println("The City Which Is 52 From Zurich is : "+cityNameDistanceFromZurich);
-
-
+        String cityNameDistanceFromZurich = sortingCities.findTheCityDistanceFromZurich52(cityName, cityDistance);
+        System.out.println();
+        System.out.println("The City Which Is 52 From Zurich is : " + cityNameDistanceFromZurich);
+        ;
+        String theCitiesFromZurichMoreThanAndEqualTo270 = sortingCities.findTheCitiesFromZurichMoreThanAndEqualTo270(cityName, cityDistance);
+        System.out.println("The cities which are greater than and equal to 270 km fromZurich is :" + theCitiesFromZurichMoreThanAndEqualTo270);
     }
 
     /**
      * This Method is used to change names in Upper case
+     *
      * @param name-string variable
      * @return string-in upper case
      */
@@ -34,15 +34,22 @@ public class SortingCities
         String upperCaseName = name.toUpperCase();
         return upperCaseName;
     }
-    public String findTheCityDistanceFromZurich52(String [] cityName,String[] cityDistance)
-    {
-        for (int index = 0; index <cityName.length ; index++)
-        {
-            if(cityDistance[index].equals("52"))
-            {
-                 return cityName[index];
+
+    public String findTheCityDistanceFromZurich52(String[] cityName, String[] cityDistance) {
+        for (int index = 0; index < cityName.length; index++) {
+            if (cityDistance[index].equals("52")) {
+                return cityName[index];
             }
         }
-       return "";
+        return "";
+    }
+
+    public String findTheCitiesFromZurichMoreThanAndEqualTo270(String[] cityName, String[] cityDistance) {
+        for (int index = 0; index < cityName.length; index++) {
+            if (cityDistance[index].endsWith("270")) {
+                return cityName[index];
+            }
+        }
+        return"";
     }
 }
