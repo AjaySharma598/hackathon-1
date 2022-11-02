68ca5ffc91d151dd1c54b8f72929584291e2bdb9
@ajaydixit598 <ajaydixit598@gmail.com>
2022-11-02 09:57:41 +0530

    convert to upper case method is created

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -10,5 +10,16 @@ public class SortingCities
     {
         String []cityName={"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch" };
         int[] cityDistance={ 138, 52 , 118 , 136 ,85 ,276 ,103 ,87 ,214 ,101};
+        SortingCities sortingCities=new SortingCities();
+        System.out.println("The city Name in Upper Case : ");
+        for (int index = 0; index <cityName.length ; index++) {
+
+            String upperCaseCityName=sortingCities.convertToUpperCase(cityName[index]);
+            System.out.print( " " +upperCaseCityName +",");
+        }
+    }
+    public String convertToUpperCase(String name) {
+        String upperCaseName = name.toUpperCase();
+        return upperCaseName;
     }
 }
