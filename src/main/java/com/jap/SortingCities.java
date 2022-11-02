74dfca8ab2827cfda6e6ca14fcfefe237bd0595d
@ajaydixit598 <ajaydixit598@gmail.com>
2022-11-02 12:34:35 +0530

    some changes in findTheCityDistanceFromZurich52 method

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -9,7 +9,7 @@ public class SortingCities
     public static void main( String[] args )
     {
         String []cityName={"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch" };
-        int[] cityDistance={ 138, 52 , 118 , 136 ,85 ,276 ,103 ,87 ,214 ,101};
+        String[] cityDistance={ "138" , "52" , "118" , "136" ,"85" ,"276" ,"103" ,"87" ,"214" ,"101"};
         SortingCities sortingCities=new SortingCities();
         System.out.println("The city Name in Upper Case : ");
         for (int index = 0; index <cityName.length ; index++) {
@@ -18,19 +18,32 @@ public class SortingCities
             System.out.print( " " +upperCaseCityName +",");
         }
 
-           String cityNameDistanceFromZurich= sortingCities.findTheCityDistanceFromZurich52(cityDistance);
+           String cityNameDistanceFromZurich= sortingCities.findTheCityDistanceFromZurich52(cityName,cityDistance);
 
         System.out.println("The City Which Is 52 From Zurich is : "+cityNameDistanceFromZurich);
+
+
     }
+
+    /**
+     * This Method is used to change names in Upper case
+     * @param name-string variable
+     * @return string-in upper case
+     */
     public String convertToUpperCase(String name) {
         String upperCaseName = name.toUpperCase();
         return upperCaseName;
     }
-    public String findTheCityDistanceFromZurich52(int[] cityDistance)
+    public String findTheCityDistanceFromZurich52(String [] cityName,String[] cityDistance)
     {
-
-
-
-        return "";
+        //String findCity=cityDistance[0];
+        for (int index = 0; index <cityDistance.length ; index++)
+        {
+            if(cityDistance.equals("52"))
+            {
+                 return cityName[index];
+            }
+        }
+       return "";
     }
 }
--- src/test/java/com/jap/SortingCitiesTest.java
+++ src/test/java/com/jap/SortingCitiesTest.java
@@ -2,10 +2,13 @@ package com.jap;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SortingCitiesTest {
+
+    SortingCities sortingCities;
 
     @Before
     public void setUp()  {
@@ -13,6 +16,12 @@ public class SortingCitiesTest {
 
     @After
     public void tearDown()  {
+    }
+
+    @Test
+    public void convertToUpperCase()
+    {
+
     }
 
     //write all the test cases here
